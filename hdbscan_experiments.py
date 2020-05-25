7ae82d2dc22eccde58162797341e79c597f205cf
gabrielrmachado <gabriel.rmachado10@gmail.com>
2020-05-24 21:09:27 -0300

    First commit

---
+++ hdbscan_experiments.py
@@ -0,0 +1,43 @@
+import hdbscan
+import csv
+import os
+
+class Point:
+    def __init__(self, x, y, cl):
+        self.x = x
+        self.y = y
+        self.cl = cl
+
+class Experiment(object):
+    dataset = []
+
+    @staticmethod
+    def read_csv(dataset_name):
+        file = open(os.path.join("datasets", dataset_name))
+        reader = csv.reader(file)
+
+        i = 1
+        for col in reader:
+            Experiment.dataset.append(Point(col[0], col[1], col[2]))
+            i = i + 1
+
+    @staticmethod
+    def print_dataset():
+        size = len(Experiment.dataset)
+        if size == 0: print("No dataset read yet...")
+        else:
+            i = 1
+            for data in Experiment.dataset:
+                print("Point {0}: x: {1}, y: {2}, class: {3}".format(i, data.x, data.y, data.cl))
+                i = i + 1
+
+if __name__ == "__main__":
+    Experiment.read_csv("Aggregation.csv")
+    Experiment.print_dataset()
+
+# data, _ = make_blobs(1000)
+
+# clusterer = hdbscan.HDBSCAN(min_cluster_size=10)
+# cluster_labels = clusterer.fit_predict(data)
+
+# print(cluster_labels)
