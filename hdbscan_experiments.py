ad50c2bfb9e24d6e514ea133abc4edb5844aa637
gabrielrmachado <gabriel.rmachado10@gmail.com>
2020-05-24 22:28:48 -0300

    DBSCAN implementation tested.

--- hdbscan_experiments.py
+++ hdbscan_experiments.py
@@ -1,7 +1,9 @@
-import hdbscan
+from hdbscan import HDBSCAN
+from sklearn.cluster import DBSCAN
 import numpy as np
 import pandas as pd
 from sklearn.datasets import make_blobs
+import sklearn.metrics as metrics
 import csv
 import os
 
@@ -14,10 +16,15 @@ class Experiment(object):
 
     @staticmethod
     def run_hdbscan(dataset, min_cluster_size):
-        clusterer = hdbscan.HDBSCAN(min_cluster_size)
+        clusterer = HDBSCAN(min_cluster_size)
         return clusterer.fit_predict(dataset)
+
+    def run_dbscan(dataset, min_cluster_size, eps):
+        return DBSCAN(eps = eps, min_samples = min_cluster_size).fit_predict(dataset)
 
 if __name__ == "__main__":
     data, classes = Experiment.read_csv("Aggregation.csv")
-    clusterer_labels = Experiment.run_hdbscan(data, 7)
-    print(clusterer_labels)
+    h_clusterer_labels = Experiment.run_hdbscan(data, 7)
+    d_clusterer_labels = Experiment.run_dbscan(data, 20, 7)
+    print(metrics.fowlkes_mallows_score(classes, h_clusterer_labels))
+    print(metrics.fowlkes_mallows_score(classes, d_clusterer_labels))
