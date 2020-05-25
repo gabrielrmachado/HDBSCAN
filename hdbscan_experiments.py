44e92d8d8af4eece9906dbffdb8369d1eef25023
gabrielrmachado <gabriel.rmachado10@gmail.com>
2020-05-24 22:11:51 -0300

    HDBSCAN implementation tested.

--- hdbscan_experiments.py
+++ hdbscan_experiments.py
@@ -10,27 +10,14 @@ class Experiment(object):
     @staticmethod
     def read_csv(dataset_name):
         df = pd.read_csv(os.path.join("datasets", dataset_name), sep=',', header = 0)
-        return df.values
+        return df.values[:, :2], df.values[:, 2]
 
     @staticmethod
     def run_hdbscan(dataset, min_cluster_size):
         clusterer = hdbscan.HDBSCAN(min_cluster_size)
-        clusterer_labels = clusterer.fit_predict(dataset)
-
-        print(clusterer_labels)
+        return clusterer.fit_predict(dataset)
 
 if __name__ == "__main__":
-    dataset = Experiment.read_csv("Aggregation.csv")
-    data = dataset[:, :2]
-    classes = dataset[:, 2]
-    print(data)
-    print(classes)
-    # Experiment.run_hdbscan(7)
-
-    # data, _ = make_blobs(1000)
-
-    # clusterer = hdbscan.HDBSCAN(min_cluster_size=10)
-    # cluster_labels = clusterer.fit_predict(data)
-
-    # # print(cluster_labels)
-    # print(data)
+    data, classes = Experiment.read_csv("Aggregation.csv")
+    clusterer_labels = Experiment.run_hdbscan(data, 7)
+    print(clusterer_labels)
